e6d8a200b8475ff4c4f2a913fafc9f7a573319b7
Jason Doo <jeong7496@gmail.com>
2018-09-15 16:21:05 -0700

    Delete files

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,6 +31,3 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-PLUGIN_PATHS = ['plugins']
-PLUGINS = ['assets', 'neighbors']
