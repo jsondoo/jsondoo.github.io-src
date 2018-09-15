2d88176530072b824b315d967de799c3feeeda67
Jason Doo <jeong7496@gmail.com>
2018-09-15 15:22:06 -0700

    first commit ?

---
+++ content/cpsc313.md
@@ -0,0 +1,5 @@
+Title: CPSC 313 Course Review
+Date: 2018-09-15 15:07
+Category: Review
+
+Following is a review of my favorite mechanical keyboard.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Jason Doo'
+SITENAME = 'Jason Doo'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Dawson'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Github', 'http://github.com/jsondoo'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('Github', 'http://github.com/jsondoo'),
+          ('LinkedIn', 'http://linkedin.com/in/jason-doo'),)
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
