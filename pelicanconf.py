594b1a94a4c472a0fa022366b3ecdee42b5b4a88
Jason Doo <jeong7496@gmail.com>
2018-09-15 15:56:22 -0700

    add plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,3 +31,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['assets', 'neighbors']
